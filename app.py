8998f68807bd9cf74aac3680fc046d43381bb7be
unknown <rick107418@gmail.com>
2025-10-29 15:05:16 +0800

    更新 Flex Message + 圖片路徑更正

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, FlexSendMessage
+# TextSendMessage,
 import os
 import json
 
